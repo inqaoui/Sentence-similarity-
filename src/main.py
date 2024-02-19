f500851903eec2114289311a9b02416535ba5908
Mehdi <inqaouimehdi@gmail.com>
2024-02-19 13:39:54 +0100

    v3

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+from Similarity import model
+from fastapi import FastAPI
 
 m=model.Similarity()
 a=m.compute("The cat sits outside","A man is playing guitar")
-print(float(a.item()))
+# print(float(a.item()))
+app = FastAPI ()
+@app . route ("/")
+def home () :
+    return {"Hello world"}
